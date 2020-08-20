473a37bc0bf9bd1097117cef6884f73ee2732b97
diegosalazar343 <salazardiego343@gmail.com>
2020-08-20 10:20:21 -0700

    add div container and form for user input in html, add two tests under behavior order in scripts
    Co-authored-by:  Jillian Gibson <jillian.l.gibson@gmail.com>
    Co-authored-by: Garrett Brown <garrettpaulbrown@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// Behavior Order:
+// Test: The program convert the user input from a number to a Roman numeral.
+// Input: 2
+// Output: II
+
+// Test: The program will switch to subtraction when four or more of the same Roman numeral are in a row.
+// Input: 4
+// Output: IV
+
+// Test:
+// Input:
+// Output:
+
+// Test:
+// Input:
+// Output:
+
+// Business Interface
+
+
+
+// User Interface
