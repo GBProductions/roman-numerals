dbaf5fb209f3eb9c282bdd54430b51cb1d595cc9
diegosalazar343 <salazardiego343@gmail.com>
2020-08-20 10:41:45 -0700

    add two more tests under Behavior order
    Co-authored-by:  Jillian Gibson <jillian.l.gibson@gmail.com>
    Co-authored-by: Garrett Brown <garrettpaulbrown@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 // Behavior Order:
+
+// Test: The program will send an alert if the user inputs a 0 or a number of 4000+.
+// Input: 4000
+// Output: "Please enter a number between 1 - 3999"
+
 // Test: The program convert the user input from a number to a Roman numeral.
+// Input: 1, 5, 10, 50, 100, 500, 1000
+// Output: I, V, X, L, C, D, M
+
+// Test: The program will add the values of the Roman numerals together.
 // Input: 2
 // Output: II
 
 // Test: The program will switch to subtraction when four or more of the same Roman numeral are in a row.
 // Input: 4
 // Output: IV
-
-// Test:
-// Input:
-// Output:
-
-// Test:
-// Input:
-// Output:
 
 // Business Interface
 
